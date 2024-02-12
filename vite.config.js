705b13711feef9b27cd7d9c747399c163fc9f8aa
Ethan Christensen <ethan.christensen@sdl.usu.edu>
2024-02-12 16:43:00 -0700

    Make cesiumBaseUrl absolute

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const cesiumSource = "node_modules/cesium/Build/Cesium";
 // This is the base url for static files that CesiumJS needs to load.
-// Set to an empty string to place the files at the site's root path
-const cesiumBaseUrl = "cesiumStatic";
+// Set to "/" to place the files at the site's root path
+const cesiumBaseUrl = "/cesiumStatic";
 
 // https://vitejs.dev/config/
 export default defineConfig({
