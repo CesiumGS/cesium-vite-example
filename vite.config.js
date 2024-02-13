2c458c9502bf7ce57eaae2d0b7fa7d6b63d5cb81
Ethan Christensen <ethan.christensen@sdl.usu.edu>
2024-02-13 13:28:36 -0700

    Use absolute path only for CESIUM_BASE_URL

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const cesiumSource = "node_modules/cesium/Build/Cesium";
 // This is the base url for static files that CesiumJS needs to load.
-// Set to "/" to place the files at the site's root path
-const cesiumBaseUrl = "/cesiumStatic";
+// Set to an empty string to place the files at the site's root path
+const cesiumBaseUrl = "cesiumStatic";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     // Define relative base path in cesium for loading assets
     // https://vitejs.dev/config/shared-options.html#define
-    CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl),
+    CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
   },
   plugins: [
     // Copy Cesium Assets, Widgets, and Workers to a static directory.
